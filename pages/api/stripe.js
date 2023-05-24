d087137fea1e9ec0c3f5f45454e3f8274bbe9c75
nighthowler27 <128471906+nighthowler27@users.noreply.github.com>
2023-05-24 10:21:23 +0800

    update payment method, fix error and bug

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,6 +2,9 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
@@ -11,15 +14,16 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1NAtq7KnJAn16Vl9bltUV51i' },
+          { shipping_rate: 'shr_1NAttnKnJAn16Vl9EWuw0lPc' }
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/wt4orogt/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
-              currency: 'usd',
+              currency: 'php',
               product_data: { 
                 name: item.name,
                 images: [newImage],
